86830714a2fc30280390268c2da5b946f6690884
WestonElliott <Westonini101@gmail.com>
2019-06-12 16:17:35 -0700

    - Added muzzle flash lighting and particle effect when shooting - Added ammo and reload to pistol with UI

--- JamCraft 3 Entry/Assets/Scripts/PlayerShooting.cs
+++ JamCraft 3 Entry/Assets/Scripts/PlayerShooting.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
 
 public class PlayerShooting : MonoBehaviour
 {
@@ -6,12 +9,45 @@ public class PlayerShooting : MonoBehaviour
 
     public Transform shootPosition;
 
+    public GameObject muzzleFlashLight;
+    public ParticleSystem muzzleFlashParticles;
+
+    private int bulletsInMag = 6;
+    private int bulletsInPouch = 30;
+    private float reloadTime = 1.5f;
+    private bool reloadTimeActive = false;
+
+    public TextMeshProUGUI ammoText;
+    public TextMeshPro reloadText;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        ammoText.text = "Ammo: \n" + bulletsInMag.ToString() + " / " + bulletsInPouch.ToString();
+
+        if (Input.GetButtonDown("Fire1") && bulletsInMag != 0 && reloadTimeActive == false) //Shoot Input
         {
             Shoot();
+        }
+
+        if (Input.GetButtonDown("Reload") && bulletsInPouch > 0 && bulletsInMag != 6) //Reload Input
+        {
+            reloadTimeActive = true;
+        }
+
+        if (reloadTimeActive == true) // If the player is reloading
+        {
+            reloadText.text = "Reloading...";
+            reloadTime -= Time.deltaTime;
+
+            if (reloadTime <= 0)
+            {
+                Reload();
+
+                reloadTimeActive = false;
+                reloadText.text = "";
+                reloadTime = 1.5f;
+            }
         }
     }
 
@@ -21,5 +57,29 @@ public class PlayerShooting : MonoBehaviour
         bulletInstance = Instantiate(bulletPrefab, shootPosition.position, shootPosition.rotation) as Rigidbody;
         bulletInstance.AddForce(shootPosition.forward * 1000);
         Destroy(bulletInstance.gameObject, 0.5f);
+        StartCoroutine(ToggleMuzzleFlash());
+        bulletsInMag -= 1;
+    }
+
+    void Reload()
+    {
+        int newBulletsInMag;
+        newBulletsInMag = 6 - bulletsInMag;
+        bulletsInMag += newBulletsInMag;
+        bulletsInPouch -= newBulletsInMag;
+        reloadText.text = "";
+
+        if (bulletsInPouch < 0)
+        {
+            bulletsInPouch = 0;
+        }
+    }
+
+    private IEnumerator ToggleMuzzleFlash()
+    {
+        muzzleFlashParticles.Play();
+        muzzleFlashLight.SetActive(true);
+        yield return new WaitForSeconds(0.05f);
+        muzzleFlashLight.SetActive(false);
     }
 }
