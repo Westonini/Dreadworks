a0d1ce6d6191b10b80e0a2bf8364ab7ddcd2cd69
WestonElliott <Westonini101@gmail.com>
2019-06-23 02:38:53 -0700

    - Added instructions/goal UI, gameobjects, and scripts

---
+++ JamCraft 3 Entry/Assets/Scripts/UISCripts/InstructionsManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InstructionsManager : MonoBehaviour
+{
+    public bool newTextAdded = false;
+
+    public TextMeshProUGUI instructionsText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (newTextAdded)
+        {
+            CancelInvoke("RemoveInstructionsText");
+            Invoke("RemoveInstructionsText", 10);
+            newTextAdded = false;
+        }
+    }
+
+    public void RemoveInstructionsText()
+    {
+        instructionsText.text = "";
+    }
+}
---
+++ JamCraft 3 Entry/Assets/Scripts/UISCripts/ShowInstructions.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowInstructions : MonoBehaviour
+{
+    private Collider _collider;
+
+    public TextMeshProUGUI instructionsText;
+    public string instructions;
+
+    public TextMeshProUGUI goalsText;
+    public string[] newGoal;
+
+    InstructionsManager IM;
+
+    void Awake()
+    {
+        IM = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<InstructionsManager>();
+    }
+
+    void Start()
+    {
+        _collider = GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //Tell the InstructionsManager script that new text has been added (used for removing the text).
+            IM.newTextAdded = true;
+
+            //Changes instructionsText to whatever instruction was inputted in the field in the inspector.
+            instructionsText.text = instructions;
+
+            //For each new goal, add them to the goal list.
+            for (int i = 0; i < newGoal.Length; i++)
+            {
+                goalsText.text += newGoal[i] + "\n";
+            }
+
+            //Disable the collider once the player interacts with it.
+            _collider.enabled = false;
+        }
+    }
+}
